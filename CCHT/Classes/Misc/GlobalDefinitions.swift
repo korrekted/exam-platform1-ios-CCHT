b958196f1357aa6b0b6f1281afd7cd29e8d1bab0
akonst17 <akonst17@gmail.com>
2022-09-22 13:34:01 +0300

    интеграция sdk 2.0; новые урлы; build 5

--- CCHT/Classes/Misc/GlobalDefinitions.swift
+++ CCHT/Classes/Misc/GlobalDefinitions.swift
@@ -9,18 +9,21 @@ final class GlobalDefinitions {
     static let domainUrl = "https://exams2.mastrapi.com"
     static let apiKey = "RfSauOTHQzJHuw56"
 
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "FgDgi3rpvbnZzsut"
-
     static let applicationTag = "EXAM_CCHT"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "FgDgi3rpvbnZzsut"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "V1bSWOmxbJ1Rum7W"
 
     static let appStoreUrl = "https://apps.apple.com/us/app/ccht/id1626954794"
     static let appleAppID = "1626954794"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
